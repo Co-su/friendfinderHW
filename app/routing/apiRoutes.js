cae92e5bd2fd5404c12df082a2f807bc01f05533
Co-Su <thisusernameisnottaken@gmail.com>
2018-01-04 15:38:05 -0800

    added a lot of stuff over the course of a study session.  Still not deployable yet.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,22 @@
+var friendsData = require("../data/friends.js");
+
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+};
