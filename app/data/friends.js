bd03836c5729124520e781bf39e38bf01d1b30ef
Co-Su <thisusernameisnottaken@gmail.com>
2018-01-04 19:05:15 -0800

    Lots more stuff added, hitched on a CANNOT POST error.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,18 @@ var friendArray = [
 {
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[5,1,4,4,5,1,2,5,4,1]
+  "scores":[
+  10,
+  1,
+  2,
+  4,
+  3,
+  5,
+  8,
+  7,
+  9,
+  6
+  ]
 }
 
 module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,12 @@
-var friendsData = require("../data/friends.js");
+var friendsData = require("../data/friends");
 
 
 module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
-    res.json(tableData);
+    res.json(friendsData);
   });
-
   app.post("/api/friends", function(req, res) {
-
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+    friendsData.push(req.body);
   });
-
 };
